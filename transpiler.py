dd157adbaa28519748dca21d68ed4c6912a0660b
David Purdum <purdum41@gmail.com>
2018-07-29 15:04:26 -0400

    Print implicit rutrm in singleline html

--- transpiler.py
+++ transpiler.py
@@ -7,7 +7,7 @@ depth = []
 currentTag = 0
 
 # if a tag name is seen, but a { has not yet been seen, then this value is false
-expected = True
+expectTag = True
 
 def initialize(filename):
     global file
@@ -21,15 +21,15 @@ def compile_next():
     global output
     global depth
     global currentTag
-    global expected
+    global expectTag
 
     token = file.next_token()
 
     if token == "{":
-        write_tag()
-        expected = True
+        write_tag(False)
+        expectTag = True
     elif token == "}":
-        end_tag()
+        end_tag(False)
     elif token == ".":
         currentTag.add_class(file.next_token())
     elif token == "#":
@@ -39,23 +39,25 @@ def compile_next():
     elif token == "|":
         compile_attribute()
     elif token == "\"":
-        if not expected:
+        if not expectTag:
             currentTag.is_implicit = True
-            write_tag()
-            compile_string()
-            end_tag()
-            expected = True
+            write_tag(True)
+            compile_string(True)
+            end_tag(True)
+            expectTag = True
         else:
-            compile_string()
+            compile_string(False)
     elif token == "":
         # End of file
         return ""
+    elif token == " ":
+        0
     else:
         # Must be a tag name
-        if not expected:
+        if not expectTag:
             currentTag.is_implicit = True
-            write_tag()
-        expected = False
+            write_tag(True)
+        expectTag = False
         currentTag = Tag(token)
 
 
@@ -84,31 +86,43 @@ def compile_attribute():
         currentTag.add_attribute(name, "")
         file.undo_token()
 
-def compile_string():
+def compile_string(inline):
     global file
     global output
     
-    output.write(get_tabs() + file.next_token() + "\n")
+    if inline:
+        output.write(file.next_token())
+    else:
+        output.write(get_tabs() + file.next_token() + "\n")
     file.next_token() # Expect "
 
-def write_tag():
+def write_tag(inline):
     global currentTag
     global depth
     global output
 
     depth.append(currentTag)
-    output.write(get_tabs() + currentTag.get_start_tag() + "\n")
+    if inline:
+        output.write(get_tabs() + currentTag.get_start_tag())
+    else:
+        output.write(get_tabs() + currentTag.get_start_tag() + "\n")
     inc_tabs()
 
-def end_tag():
+
+def end_tag(inline):
     global depth
+    global output
+
     dec_tabs()
-    output.write(get_tabs() + depth.pop(-1).get_end_tag() + "\n")
+    if inline:
+        output.write(depth.pop().get_end_tag() + "\n")
+    else:
+        output.write(get_tabs() + depth.pop().get_end_tag() + "\n")
     if len(depth) > 1:
         if depth[-1].is_implicit:
             end_tag()
 
-# --- Controls the tabing in html output
+# --- Controls the tabbing in html output
 
 tabs = 0
 
